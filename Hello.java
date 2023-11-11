c3af14cd8c5351a239d3b124712aff50294b5bd7
Abirami-02 <147965516+Abirami-02@users.noreply.github.com>
2023-11-11 10:10:38 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        int num=10;
+        int tea=20;
+        System.out.println("Hello world");
+        System.out.println(3+5);
+        System.out.println(6+6);
+        System.out.println(num);
+    }
+}
