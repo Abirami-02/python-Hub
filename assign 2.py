509ac14edf89942b764f260809432e6b6c56e3f3
Abirami-02 <147965516+Abirami-02@users.noreply.github.com>
2023-11-11 11:10:01 +0530

    Add files via upload

---
+++ assign 2.py
@@ -0,0 +1,10 @@
+list=[];
+print([i for i in range(1,50) if(i%2!=0) and (i%3==0)])
+# list of numbers odd numbers from 1to 10
+list2=[]
+for i in range(1,10):
+    if(i%2!=0):
+        list2.append(i)
+print(list2)
+        
+       
